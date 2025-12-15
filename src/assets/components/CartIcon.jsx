892441632b5357cd9bfddac5331dd83b908fbccc
asiahaws1 <144909095+asiahaws1@users.noreply.github.com>
2025-12-15 13:42:56 -0700

    added a couple new components and pages

---
+++ src/assets/components/CartIcon.jsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+export default function CartIcon() {
+    return (
+        <div className="cart-cion">
+            <FontAwesomeIcon icon={byPrefixAndName.fas['cart-shopping']} />
+        </div>
+    );
+}
---
+++ src/assets/components/Products.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+export default function Products() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then(response => response.json())
+            .then(data => {
+                setProducts(data);
+            })
+            .catch(err => {
+                console.error("Get Products Error: ", err);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>Products</h1>
+            <div className="product-card">
+                {products.map(product => (
+                    <div key={product.id}>
+                        <h2>{product.title}</h2>
+                        <h2>{product.price}</h2>
+                        <h2>{product.description}</h2>
+                        <h2>{product.category}</h2>
+                        <img src={product.image}></img>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
